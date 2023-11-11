1a31695a6d808f02672d8430e13e33997da37c0d
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-11 23:16:44 +0900

    feat: CORS 설정

---
+++ src/main/java/com/sehee/heesinsa/MvcConfiguration.java
@@ -0,0 +1,11 @@
+package com.sehee.heesinsa;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/*").allowedOrigins("*");
+    }
+}
