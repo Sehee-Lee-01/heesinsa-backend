bb0fe795493a2a1792c1c48a3219452fa8f89bf0
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-11 23:13:41 +0900

    chore: 프로젝트 초기화 설정

---
+++ src/main/java/com/sehee/heesinsa/HeesinsaApplication.java
@@ -0,0 +1,13 @@
+package com.sehee.heesinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeesinsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeesinsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sehee/heesinsa/HeesinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehee.heesinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeesinsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
