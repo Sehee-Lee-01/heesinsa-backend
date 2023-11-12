459bb98a50dd351d92046b43646fe3bfd41c8ecd
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-12 23:17:51 +0900

    feat: NoSuchElementException 예외 핸들러 구현

--- src/main/java/com/sehee/heesinsa/exception/GlobalControllerAdvice.java
+++ src/main/java/com/sehee/heesinsa/exception/GlobalControllerAdvice.java
@@ -5,10 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 public class GlobalControllerAdvice {
     @ExceptionHandler(value = ConstraintViolationException.class)
     public ResponseEntity<String> handleValidationError(ConstraintViolationException e) {
         return ResponseEntity.badRequest().body("Double-check your input requirements.");
+    }
+
+    @ExceptionHandler(value = NoSuchElementException.class)
+    public ResponseEntity<String> handleValidationError(NoSuchElementException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
